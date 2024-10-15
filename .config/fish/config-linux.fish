8140039bc91795661295895250e68823a3a902cd
Deepak22903 <deepakshitole4@gmail.com>
2024-10-15 18:11:14 +0530

    Updated

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -19,6 +19,8 @@ alias ydlmp4='yt-dlp -f "bestvideo&#91;ext=mp4]+bestaudio&#91;ext=m4a]/best&#91;
 alias ydlmkv='yt-dlp -f "bestvideo&#91;ext=mkv]+bestaudio&#91;ext=mka]/best&#91;ext=mkv]/best"'
 alias back="/home/deepak/ghq/github.com/Deepak22903/My_Shell_Scripts/global/backup_data.sh"
 alias batt="cat /sys/class/power_supply/BAT0/capacity"
+alias upnet="/home/deepak/ghq/github.com/Deepak22903/My_Shell_Scripts/global/connect_internet.sh"
+alias downnet="/home/deepak/ghq/github.com/Deepak22903/My_Shell_Scripts/global/disconnect_internet.sh"
 
 function copyErrors
     $argv 2>&1 | xclip -selection clipboard
