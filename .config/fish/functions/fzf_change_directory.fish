ee55cacd1110e6e7116a7a8836ca481e906766b6
Deepak22903 <deepakshitole4@gmail.com>
2024-11-29 18:31:07 +0530

    Updated

--- .config/fish/functions/fzf_change_directory.fish
+++ .config/fish/functions/fzf_change_directory.fish
@@ -1,5 +1,18 @@
+# Define a global variable for excluded directories
+set -g EXCLUDED_DIRS ".git .cache node_modules .rustup"
+
+# Helper function to generate exclusion pattern
+function _generate_exclude_pattern
+    set exclude_pattern ""
+    for dir in $EXCLUDED_DIRS
+        set exclude_pattern "$exclude_pattern -not -path \"./$dir/*\""
+    end
+    echo $exclude_pattern
+end
+
+# Function to perform directory navigation with fzf
 function _fzf_change_directory
-    if [ (count $argv) ]
+    if [ (count $argv) -gt 0 ]
         set fzf_flags --height 40% --border --query "$argv "
     else
         set fzf_flags --height 40% --border
@@ -14,10 +27,11 @@ function _fzf_change_directory
     end
 end
 
+# Main function to list directories and invoke _fzf_change_directory
 function fzf_change_directory
     begin
-        # List directories recursively from current directory
-        find . -type d | grep -v '\.git' | sed -e 's/^\.\///'
+        # Use find with exclusion pattern
+        eval find . -type d (_generate_exclude_pattern) | sed -e 's|^\./||'
     end | _fzf_change_directory $argv
     commandline -f execute
 end
