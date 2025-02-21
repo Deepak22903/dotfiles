6e6c26330e0455832799dfc1739392d9590b74c6
Deepak22903 <deepakshitole4@gmail.com>
2025-02-21 12:42:27 +0530

    Updated

--- nvim/.config/nvim.nvchad.bak/lua/custom/init.lua
+++ nvim/.config/nvim.nvchad.bak/lua/custom/init.lua
@@ -22,7 +22,7 @@ autocmd("BufReadPost", {
   end,
 })
 
-vim.api.nvim_create_autocmd("BufDelete", {
+autocmd("BufDelete", {
   callback = function()
     local bufs = vim.t.bufs
     if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
