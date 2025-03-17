0e2bd98a395569dcfd9d5b9a71c393724e07c136
Deepak22903 <deepakshitole4@gmail.com>
2025-03-17 22:48:52 +0530

    Updated

--- nvim/.config/nvim/lua/plugins/transparent.lua
+++ nvim/.config/nvim/lua/plugins/transparent.lua
@@ -1,7 +1,29 @@
 return {
   {
     "xiyaowong/transparent.nvim",
-    enabled = false,
+    enabled = true,
     -- Optional, you don't have to run setup.
+
+    extra_groups = { -- table/string: additional groups that should be cleared
+      "BufferLineTabClose",
+      "BufferlineBufferSelected",
+      "BufferLineFill",
+      "BufferLineBackground",
+      "BufferLineSeparator",
+      "BufferLineIndicatorSelected",
+
+      "IndentBlanklineChar",
+
+      -- make floating windows transparent
+      "LspFloatWinNormal",
+      "Normal",
+      "NormalFloat",
+      "FloatBorder",
+      "TelescopeNormal",
+      "TelescopeBorder",
+      "TelescopePromptBorder",
+      "SagaBorder",
+      "SagaNormal",
+    },
   },
 }
