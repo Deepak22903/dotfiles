cf079193d3aa01c631a506b3c5dc3a9017d07ae1
Deepak22903 <deepakshitole4@gmail.com>
2025-04-02 15:27:11 +0530

    added yazi plugins

---
+++ instalScripts/yazi.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# open with cmd plugin
+ya pack -a Ape/open-with-cmd
+
+# jump to char plugin
+ya pack -a yazi-rs/plugins:jump-to-char
+
+# bookmarks plugin
+ya pack -a dedukun/bookmarks
+
+# chmod plugin
+ya pack -a yazi-rs/plugins:chmod
+
+# archivemount plugin
+ya pack -a AnirudhG07/archivemount
+
+# rsync plugin
+ya pack -a GianniBYoung/rsync
+
+# restore plugin
+ya pack -a boydaihungst/restore
+
+# simple mtpfs plugin
+ya pack -a boydaihungst/simple-mtpfs
