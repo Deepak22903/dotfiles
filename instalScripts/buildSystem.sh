8f8fb15b2696cf79a862a8d247bfd10cc6e4540f
Deepak22903 <deepakshitole4@gmail.com>
2025-05-29 13:13:32 +0530

    Updated

--- instalScripts/buildSystem.sh
+++ instalScripts/buildSystem.sh
@@ -289,3 +289,6 @@ set -l _tide_tmp_dir (command mktemp -d)
 curl https://codeload.github.com/ilancosman/tide/tar.gz/v6 | tar -xzC $_tide_tmp_dir
 command cp -R $_tide_tmp_dir/*/{completions,conf.d,functions} $__fish_config_dir
 fish_path=(status fish-path) exec $fish_path -C "emit _tide_init_install"
+
+yay -S bat tldr ani-cli eza linux-wifi-hotspot zoxide yt-dlp tty-clock sshfs python-pipx fzf tmux shell-color-scripts  wl-clipboard rofi-wayland
+
